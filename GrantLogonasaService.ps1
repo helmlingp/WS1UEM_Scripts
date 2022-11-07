d5f2c9f4db4c89e12467dd1c767831bd9d4a34d1
Phil Helmling <helmlingp@vmware.com>
2022-11-07 14:56:53 +1030

    added GrantLogonasaService.ps1

---
+++ GrantLogonasaService.ps1
@@ -0,0 +1,136 @@
+<#
+  .Synopsis
+    This powershell script grants the "Log on as a Service" User Rights Assignment to the user specified by the $ServiceAccount param
+    Created:   	    Nov, 2022
+    Created by:	    Phil Helmling, @philhelmling
+    Organization:   VMware, Inc.
+    Filename:       GrantLogonasaService.ps1
+    Github:         https://github.com/helmlingp/WS1UEM_Scripts
+  .DESCRIPTION
+    Grants the "Log on as a Service" User Rights Assignment to the user specified by the $ServiceAccount parameter.
+    Can be deployed as a UEM Script or Application (Script inside ZIP). Ensure you enable the param section below if deploying as script within a ZIP
+
+  .REQUIREMENTS
+    Deployed in SYSTEM CONTEXT as a Workspace ONE UEM Script
+  .EXAMPLE
+    Create a Workspace ONE UEM Script under Resources > Scripts > ADD > Windows
+    Language = Powershell
+    Execution Context & Privileges = SYSTEM CONTEXT
+    Execution Architecture =  Auto
+    Timeout = 30
+#>
+<# param (
+    [Parameter(Mandatory=$true)][string]$ServiceAccount
+)
+ #>
+
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+
+public enum LSA_AccessPolicy : long
+{
+    // Other values omitted for clarity
+    POLICY_ALL_ACCESS = 0x00001FFFL
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct LSA_UNICODE_STRING
+{
+    public UInt16 Length;
+    public UInt16 MaximumLength;
+    public IntPtr Buffer;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct LSA_OBJECT_ATTRIBUTES
+{
+    public UInt32 Length;
+    public IntPtr RootDirectory;
+    public LSA_UNICODE_STRING ObjectName;
+    public UInt32 Attributes;
+    public IntPtr SecurityDescriptor;
+    public IntPtr SecurityQualityOfService;
+}
+
+public static partial class AdvAPI32 {
+    [DllImport("advapi32.dll", SetLastError = true, PreserveSig = true)]
+    public static extern uint LsaOpenPolicy(
+        ref LSA_UNICODE_STRING SystemName,
+        ref LSA_OBJECT_ATTRIBUTES ObjectAttributes,
+        uint DesiredAccess,
+        out IntPtr PolicyHandle);
+
+    [DllImport("advapi32.dll")]
+    public static extern Int32 LsaClose(IntPtr ObjectHandle);
+
+    [DllImport("advapi32.dll", SetLastError = true, PreserveSig = true)]
+    public static extern uint LsaAddAccountRights(
+        IntPtr PolicyHandle,
+        byte[] AccountSid,
+        LSA_UNICODE_STRING[] UserRights,
+        uint CountOfRights);
+}
+'@
+
+function Get-LsaPolicyHandle() {
+  $system = New-Object LSA_UNICODE_STRING
+  $attrib = New-Object LSA_OBJECT_ATTRIBUTES -Property @{
+      Length = 0
+      RootDirectory = [System.IntPtr]::Zero
+      Attributes = 0
+      SecurityDescriptor = [System.IntPtr]::Zero
+      SecurityQualityOfService = [System.IntPtr]::Zero
+  };
+
+  $handle = [System.IntPtr]::Zero
+
+  $hr = [AdvAPI32]::LsaOpenPolicy([ref] $system, [ref]$attrib, [LSA_AccessPolicy]::POLICY_ALL_ACCESS, [ref]$handle)
+
+  if (($hr -ne 0) -or ($handle -eq [System.IntPtr]::Zero)) {
+      Write-Error "Failed to open Local Security Authority policy. Error code: $hr"
+  } else {
+      $handle
+  }
+}
+
+function New-Right([string]$rightName){
+  $unicodeCharSize = 2
+  New-Object LSA_UNICODE_STRING -Property @{
+      Buffer = [System.Runtime.InteropServices.Marshal]::StringToHGlobalUni($rightName)
+      Length = $rightName.Length * $unicodeCharSize
+      MaximumLength = ($rightName.Length + 1) * $unicodeCharSize
+  }
+}
+
+function Grant-Rights([System.IntPtr]$policyHandle, [byte[]]$sid, [LSA_UNICODE_STRING[]]$rights) {
+  $result = [AdvAPI32]::LsaAddAccountRights($policyHandle, $sid, $rights, 1)
+  if ($result -ne 0) {
+      Write-Error "Failed to grant right. Error code $result"
+  } 
+}
+
+function Grant-LogonAsServiceRight([byte[]]$sid) { 
+  $logonAsServiceRightName = "SeServiceLogonRight"
+
+  try {
+      $policy = Get-LsaPolicyHandle
+      $right = New-Right $logonAsServiceRightName
+      Grant-Rights $policy $sid @($right)
+  }
+  finally {
+      if($null -ne $policy){
+          [AdvAPI32]::LsaClose($policy) | Out-Null
+      }
+  }
+}
+
+function Get-SidForUser {
+  param ([string]$UserName)
+  $sid = ((New-Object System.Security.Principal.NTAccount($UserName)).Translate([System.Security.Principal.SecurityIdentifier]))
+  [byte[]]$bytes = New-Object byte[] $sid.BinaryLength;
+  $sid.GetBinaryForm($bytes, 0);
+  return $bytes
+}
+
+Grant-LogonAsServiceRight (Get-SidForUser -UserName $env:ServiceAccount)
