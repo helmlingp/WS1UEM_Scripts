b9bda61f9e4fc0ba094a78c4034f72ed97e01d97
Phil Helmling <66004946+helmlingp@users.noreply.github.com>
2023-03-15 18:47:59 -0700

    Update GenerateLocalAdministratorPassword.ps1

--- GenerateLocalAdministratorPassword.ps1
+++ GenerateLocalAdministratorPassword.ps1
@@ -1,8 +1,8 @@
-# Description: Generate a randomized strong password and set on the local Administrator account
+# Description: Generate a randomized strong password and set on the local Administrator account. Change the password length and user using the variables
 # Execution Context: System
 # Execution Architecture: EITHER64OR32BIT
 # Timeout: 30
-# Variables: PasswordLenght,12; AdminUser,Administrator
+# Variables: PasswordLength,12; AdminUser,Administrator
 
 
 Function Invoke-GenerateStrongPassword {
@@ -13,34 +13,34 @@ Function Invoke-GenerateStrongPassword {
   Add-Type -AssemblyName System.Web
   $PassComplexCheck = $false
   do {
-  $newPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLength,1)
-  If ( ($newPassword -cmatch "[A-Z\p{Lu}\s]") `
-  -and ($newPassword -cmatch "[a-z\p{Ll}\s]") `
-  -and ($newPassword -match "[\d]") `
-  -and ($newPassword -match "[^\w]")
-  )
-  {
-  $PassComplexCheck=$True
-  }
+	$newPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLength,1)
+	  If ( ($newPassword -cmatch "[A-Z\p{Lu}\s]") `
+		-and ($newPassword -cmatch "[a-z\p{Ll}\s]") `
+		-and ($newPassword -match "[\d]") `
+		-and ($newPassword -match "[^\w]")){
+		$PassComplexCheck=$True
+	  }
   } While ($PassComplexCheck -eq $false)
-  return $newPassword
+	$securenewpwd = $newPassword | ConvertTo-SecureString -AsPlainText -Force
+	return $securenewpwd
 }
 
-Functon Invoke-SetPassword {
+Function Invoke-SetPassword {
   Param(
     [Parameter(Mandatory=$True)]
-    [string]$newpwd,
+    $securenewpwd,
     [Parameter(Mandatory=$True)]
     [string]$AdminUser
   )
-
+	
   try {
-    Set-LocalUser -Name $AdminUser -Password $newpwd -PasswordNeverExpires $false -UserMayChangePassword $false -Confirm $false
+    Set-LocalUser -Name $AdminUser -Password $securenewpwd
   }
   catch {
-    Exit 1
+    write-host "couldn't set password."
+	Exit 1
   }
 }
 
-$newpwd = Invoke-GenerateStrongPassword -PasswordLength $env:PasswordLength
-Invoke-SetPassword -newpwd $newpwd -AdminUser $env:AdminUser
+$securenewpwd = Invoke-GenerateStrongPassword -PasswordLength $env:PasswordLength
+Invoke-SetPassword -securenewpwd $securenewpwd -AdminUser $env:AdminUser
